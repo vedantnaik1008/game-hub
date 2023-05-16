d55db43a548dca1f837237cb23f3b0af2a6df23b
tallman <vedunaik@gmail.com>
2023-05-16 15:32:19 +0530

    Add emojies

---
+++ src/components/Emoji.tsx
@@ -0,0 +1,23 @@
+import bullsEye from '../assets/Emojis/bulls-eye.webp'
+import thumbsUP from '../assets/Emojis/thumbs-up.webp'
+import meh from '../assets/Emojis/meh.webp'
+import { Image, ImageProps } from '@chakra-ui/react'
+
+interface Props{
+    rating: number
+}
+
+const Emoji = ({rating}: Props) => {
+    if(rating < 3)return null;
+
+    const emojiMap: {[key: number]: ImageProps} = {
+        3: {src: meh, alt: 'meh', boxSize: '25px'},
+        4: {src: thumbsUP, alt: 'recommendede', boxSize: '25px'},
+        5: {src: bullsEye, alt: 'exceptional', boxSize: '35px'},
+    }
+  return (
+    <Image {...emojiMap[rating]} marginTop={1}/>
+  )
+}
+
+export default Emoji
