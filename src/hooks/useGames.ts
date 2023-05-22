4b9673c00d9c88a8e98839207208aaa9f2d35566
tallman <vedunaik@gmail.com>
2023-05-22 16:19:42 +0530

    implement infinite queries

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,10 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import apiClient from "../services/api-client";
+import APIClient from "../services/api-client";
 import { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
+
+const apiClient = new APIClient<Game>('/games');
 
 export interface Game{
     id: number;
@@ -12,14 +14,22 @@ export interface Game{
     metacritic: number;
     rating_top: number;
 }
-// useData<Game>('/games', {}}, [gameQuery])
 
 
-const useGames = (gameQuery: GameQuery) => useQuery<FetchResponse<Game>, Error>({
+const useGames = (gameQuery: GameQuery) => useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ['games', gameQuery],
-    queryFn: () => apiClient.get<FetchResponse<Game>>('/games',{
-        params:{genres: gameQuery.genre?.id, parent_platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder, search: gameQuery.searchText}
-    }).then(res => res.data),
-    staleTime: 24 * 60 * 60 * 1000
-})
-export default useGames;
+    queryFn: ({pageParam = 1}) =>
+      apiClient.getAll({
+        params: {
+          genres: gameQuery.genre?.id,
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText,
+          page: pageParam
+        },
+      }),
+      getNextPageParam: (lastPage, allPages) => {
+        return lastPage.next ? allPages.length + 1 : undefined;
+      }
+    })
+export default useGames
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,28 @@
-import axios from 'axios'
+import axios, { AxiosProxyConfig, AxiosRequestConfig } from 'axios'
 
 export interface FetchResponse<T>{
     count: number;
+    next: string | null;
     results: T[];
 }
 
-export default axios.create({
+const axiosInstance =  axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
         key: 'f9cd515f5be843f4a907fd4d3a6bb1d0'
     }
 })
+
+class APIClient<T>{
+    endpoint: string;
+
+    constructor(endpoint: string){
+        this.endpoint = endpoint;
+    }
+    getAll = (config: AxiosRequestConfig) => {
+        return axiosInstance.get<FetchResponse<T>>(this.endpoint, config)
+        .then(res => res.data);
+    }
+}
+
+export default APIClient;
