669afd74649135aeb4787387d72ca943aac40cfd
tallman <vedunaik@gmail.com>
2023-05-22 14:50:40 +0530

    Refactoring fetch games with React-query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 
 export interface Platform {
     id: number;
@@ -19,5 +18,11 @@ export interface Game{
 
 
 
-const useGames = (gameQuery: GameQuery) => useData<Game>('/games', {params:{genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id, ordering: gameQuery.sortOrder, search: gameQuery.searchText}}, [gameQuery])
+const useGames = (gameQuery: GameQuery) => useData<Game>('/games', {
+    params:
+    {genres: gameQuery.genre?.id,
+    platforms: gameQuery.platform?.id,
+    ordering: gameQuery.sortOrder, 
+    search: gameQuery.searchText}}, 
+    [gameQuery])
 export default useGames;
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import App from './App'
 import theme from "./theme";
 import './index.css'
 
-const queryClient = new QueryClient();
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-      <QueryClientProvider client={queryClient}>
-        
-        <App />
-      </QueryClientProvider>
+      <App />
     </ChakraProvider>
   </React.StrictMode>,
 )
