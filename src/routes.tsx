5e22aa122136d00d85add334752fb41931d52194
tallman <vedunaik@gmail.com>
2023-05-25 15:25:24 +0530

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,17 +2,22 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/layout";
 import HomePage from "./pages/HomePage";
 import GameDetailPage from "./pages/GameDetailPage";
+import ErrorPage from "./pages/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout />,
+        errorElement: <ErrorPage />,
         children: [
             {
              index: true,
-             element: <HomePage />   
+             element: <HomePage />
             },
-            {path: 'games/:id', element: <GameDetailPage />}
+            {path: 'games/:id',
+            element: <GameDetailPage /> 
+            
+            }
         ]
     }
 ])
