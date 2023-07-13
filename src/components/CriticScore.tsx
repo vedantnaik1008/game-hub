e59a65dc8901f9c27ee5b262ca60a377595cadc2
tallman <vedunaik@gmail.com>
2023-07-13 11:49:58 +0530

    changed cretic color

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -8,7 +8,7 @@ const CriticScore = ({score}: Props) => {
     let color = score > 75 ? '#2ecc71': score > 60 ? '#f1c40f' : ''; 
   return (
     <div>
-      <Badge color={color} bg={'white'}  fontSize='14px' borderRadius='4px' paddingX={2}>{score}</Badge>
+      <Badge color={color} bg={'black'}  fontSize='14px' borderRadius='4px' paddingX={2}>{score}</Badge>
     </div>
   )
 }
