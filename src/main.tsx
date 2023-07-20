2f8a81b1aa6f0a5a6b41315ede70d7b73baad8f6
tallman <vedunaik@gmail.com>
2023-07-20 10:16:46 +0530

    removed unused js

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
         <QueryClientProvider client={queryClient}>
         <RouterProvider  router={router}/>
-
         </QueryClientProvider>
       </ChakraProvider>
   </React.StrictMode>,
